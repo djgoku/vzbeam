f0057cfeb8794710dde683f7ca1ff8a5277ef7b7
Jonathan Carroll Otsuka <105506+djgoku@users.noreply.github.com>
2026-06-23 20:33:35 -0500

    feat: fetch verb

--- lib/vzbeam/cli.ex
+++ lib/vzbeam/cli.ex
@@ -7,6 +7,7 @@ defmodule VzBeam.CLI do
   Commands:
     ls                 list VM bundles
     ip <name>          print a VM's IP (from DHCP leases)
+    fetch <latest|PATH> download/cache a restore image
   """
 
   @spec main([String.t()]) :: no_return
@@ -23,5 +24,6 @@ defmodule VzBeam.CLI do
   def run(["help"]), do: {:ok, @usage}
   def run(["ip" | rest]), do: VzBeam.Commands.Ip.run(rest)
   def run(["ls" | rest]), do: VzBeam.Commands.Ls.run(rest)
+  def run(["fetch" | rest]), do: VzBeam.Commands.Fetch.run(rest)
   def run([verb | _]), do: {:error, 2, ["unknown command: ", verb, "\n", @usage]}
 end
