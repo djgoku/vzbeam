d67ddbd2fd6269435ea3b046750f9f2799c39731
Jonathan Carroll Otsuka <105506+djgoku@users.noreply.github.com>
2026-08-15 17:41:41 -0500

    chore: bump version to 0.3.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule VzBeam.MixProject do
   def project do
     [
       app: :vzbeam,
-      version: "0.3.0",
+      version: "0.3.1",
       elixir: "~> 1.17",
       escript: [main_module: VzBeam.CLI, app: nil],
       releases: releases(),
